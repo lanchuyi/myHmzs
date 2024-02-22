c3286994afa8ce688c494d02fd3bed8acae0b6e5
lanchuyi <1424625037@qq.com>
2024-02-22 12:23:11 +0800

    初始化

---
+++ src/apis/user.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+
+// 登录函数jsdoc注释，在函数的上一行写/**
+/**
+ * @description: 登录函数
+ * @param {*} data { mobile,password}
+ * @return {*} promise
+ */
+export function loginAPI(data) {
+  return request({
+    url: '/park/login',
+    method: 'POST',
+    data
+  })
+}
